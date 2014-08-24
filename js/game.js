ffa88c2688ae02662a0c9ca3ae65b8ae39c0ff62
hooman nahgiee <hooman6445@yahoo.com>
2014-08-24 11:42:27 +0430

    some simple changes

--- js/game.js
+++ js/game.js
@@ -1,17 +1,16 @@
 var c = document.getElementById("board");
 var ctx = c.getContext("2d");
-ctx.font = "16px Arial";
-var x = 1, y = 1, moveStep = 5;
-var cubeHight = 25, cubeWidth = 25;
+var x = -1, y = -1, moveStep = 5;
+var cubeHeight = 25, cubeWidth = 25;
 
 Math.floor($(document).width() /cubeWidth);
-var mainMapHight = cubeHight * Math.floor(($(document).height()-25)/cubeHight);
-var mainMapWidth = cubeWidth * Math.floor($(document).width() /cubeWidth);
+var mainMapHeight = cubeHeight * Math.floor(($(document).height()-25)/cubeHeight);
+var mainMapWidth = cubeWidth * Math.floor($(document).width()/cubeWidth);
 var speed=80;
 var gamemode="blocks";
 var isNumbersOn="Off";
 var point = 0;
-var arrayMap = [], lastchange=[], arrayBlock = [];
+var arrayMap = [], lastchange=[], Block = [];
 var colors = [ "white", "blue", "green", "red", "yellow" ];
 var borders = [ "black", "white", "gray" ];
 $("#board").mousedown(function(event) {
@@ -33,15 +32,16 @@ $(document)
 		.ready(
 				function() {
 					c.width = mainMapWidth;
-					c.height = mainMapHight+25;
-					for (var i = 0; i < mainMapHight / cubeHight; i++) {
+					c.height = mainMapHeight+25;
+					for (var i = 0; i < mainMapHeight / cubeHeight; i++) {
 						arrayMap[i] = [];
+						lastchange[i] = [];
 						for (var j = 0; j < mainMapWidth / cubeWidth; j++) {
 							arrayMap[i][j] = Math
 											.floor((Math.random() * (colors.length-1))+1);
+							lastchange[i][j] = 100;
 						}
 					}
-					lastchange=arrayMap;
 				});
 
 function chenge(i, j, arrayMap, temp, count) {
@@ -74,7 +74,7 @@ function chenge(i, j, arrayMap, temp, count) {
 	return;
 }
 function chengecond(i, j, temp) {
-	return ((i < mainMapHight / cubeHight && i >= 0
+	return ((i < mainMapHeight / cubeHeight && i >= 0
 			&& j < mainMapWidth / cubeWidth && j >= 0) && (temp.indexOf(i + ","
 			+ j) == -1));
 }
@@ -82,47 +82,47 @@ function chengecond(i, j, temp) {
 setInterval(
 		function() {
 		stats.begin();
-			ctx.clearRect(0, 0, mainMapWidth, mainMapHight+25);
+			ctx.clearRect(0, 0, mainMapWidth, mainMapHeight+25);
 			ctx.font = "16px Arial";
-			for (var i = 0; i < mainMapHight / cubeHight; i++) {
+			for (var i = 0; i < mainMapHeight / cubeHeight; i++) {
 				for (var j = 0; j < mainMapWidth / cubeWidth; j++) {
 					if(lastchange[i][j]==arrayMap[i][j]){
-						arrayBlock = arrayMap[i][j];					
-						ctx.fillStyle = (arrayBlock==0)?"white":colors[arrayBlock];
-						ctx.fillRect(j * cubeHight + 0, i * cubeWidth + 0, cubeHight,
+						Block = arrayMap[i][j];					
+						ctx.fillStyle = colors[Block];
+						ctx.fillRect(j * cubeHeight + 0, i * cubeWidth + 0, cubeHeight,
 								cubeWidth);
 						ctx.strokeStyle ="gray";
-						ctx.strokeRect(j * cubeHight + 0, i * cubeWidth + 0, cubeHight,
+						ctx.strokeRect(j * cubeHeight + 0, i * cubeWidth + 0, cubeHeight,
 								cubeWidth);
 						
 								/*text*/
 						if(isNumbersOn=="On"){
 							ctx.fillStyle = "black";
-							ctx.fillText(arrayBlock,j * cubeHight + 8 ,i * cubeWidth+18);
+							ctx.fillText(Block,j * cubeHeight + 8 ,i * cubeWidth+18);
 						}
 					}
 					lastchange=arrayMap;
 					if (((mainMapWidth/3 <= x) && (x <= (mainMapWidth/3)*2))
-							&& ((mainMapHight+0 <= y) && (y <= mainMapHight+35))) {
+							&& ((mainMapHeight+0 <= y) && (y <= mainMapHeight+35))) {
 						if(isNumbersOn=="On")
 							isNumbersOn="Off";
 						else
 							isNumbersOn="On";
-						x = 0;
-						y = 0;
+						x = -1;
+						y = -1;
 					}
 					
-					if(gamemode=="tetris"){
+					// if(gamemode=="tetris"){
 						
-					}
+					// }
 					if(gamemode=="blocks"){
-						if (((x!=0&&y!=0)&&(j * cubeWidth + 0 <= x) && (x <= j * cubeWidth + 0 + cubeWidth))
-								&& ((i * cubeHight + 0 <= y) && (y <= i * cubeHight + 0
-										+ cubeHight))) {
+						if ((((x!=0)&&(y!=0))&&(j * cubeWidth + 0 <= x) && (x <= j * cubeWidth + 0 + cubeWidth))
+								&& ((i * cubeHeight + 0 <= y) && (y <= i * cubeHeight + 0
+										+ cubeHeight))) {
 							var temp = [], count = 0;
 							chenge(i, j, arrayMap, temp, count);
-							x = 0;
-							y = 0;
+							x = -1;
+							y = -1;
 						}
 						
 						if ((arrayMap[i][j] == 0)&&chengecond(i-1,j,[])){
@@ -136,11 +136,11 @@ setInterval(
 			
 			}
 			
-			ctx.strokeStyle = 'blak';
-			ctx.strokeRect(0, 0, mainMapWidth, mainMapHight);
+			ctx.strokeStyle = 'black';
+			ctx.strokeRect(0, 0, mainMapWidth, mainMapHeight);
 			ctx.fillStyle = "black";
-			ctx.fillText("point: " + point, 10, mainMapHight+18);
+			ctx.fillText("point: " + point, 10, mainMapHeight+18);
 			ctx.fillStyle = "black";
-			ctx.fillText("numbers: " + isNumbersOn, mainMapWidth/3, mainMapHight+18);
+			ctx.fillText("numbers: " + isNumbersOn, mainMapWidth/3, mainMapHeight+18);
 		stats.end();
 		}, speed);
